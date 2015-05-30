6e516ed1b89c6ac9a88dce4b5d82d56e42e6b4ff
annapawlicka <pawlicka.anna@gmail.com>
2015-05-30 11:42:24 +0100

    Add project docs to github pages.

--- demos/clj-ev3dev-exampleapp/project.clj
+++
@@ -1,9 +0,0 @@
-(defproject clj-ev3dev-exampleapp "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
-                 [clj-ev3dev "0.1.0-SNAPSHOT"]]
-  :aot [clj-ev3dev-exampleapp.core]
-  :main clj-ev3dev-exampleapp.core)
